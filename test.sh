15804c4dd764c778672a4258ac929f9c96bc0520
Tobi Lutke <tobi@shopify.com>
2026-03-29 22:02:18 -0400

    test.sh: accept file path as argument, default to Run01_TL.MOV

--- test.sh
+++ test.sh
@@ -6,4 +6,4 @@ exec mpv --vo=gpu \
   --no-config \
   --script=telemetry.lua \
   --start=300 \
-  "/Volumes/home/Racing/Incoming/TDS Racing IMSA videos/26R02_SEBRING/D3_RACE/26IMSAR02_SEB_R_Run01_TL.MOV"
+  "${1:-/Volumes/home/Racing/Incoming/TDS Racing IMSA videos/26R02_SEBRING/D3_RACE/26IMSAR02_SEB_R_Run01_TL.MOV}"
